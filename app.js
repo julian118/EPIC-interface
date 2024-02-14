9ed51e6ca3700a6305494ab876bd1386aec6a1f4
julian <juliankroes2206@gmail.com>
2024-02-14 09:18:30 +0100

    feat: dynamic changing background image

--- app.js
+++ app.js
@@ -8,13 +8,32 @@ let seasons = {
     summer : 100,
     autumn : 100,
     winter : 100,
-    balancedImage : "images/balanced.webp",
-    springImage : "images/spring.jpg",
-    summerImage : "images/summer.webp",
-    autumnImage : "images/autumn.jpg",
-    winterImage : "images/winter.jpg"
+    balancedImage : {
+        url : "images/balanced.webp",
+        backgroundWidth : 180
+    },
+    springImage : {
+        url : "images/spring.jpg",
+        backgroundWidth : 120
+    },
+    summerImage : {
+        url : "images/summer.webp",
+        backgroundWidth : 100
+    },
+    autumnImage : {
+        url : "images/autumn.jpg",
+        backgroundWidth : 100
+    },
+    winterImage : {
+        url : "images/winter.jpg!d",
+        backgroundWidth : 100
+    }
 }
-
+function setBackground(url, width) {
+    console.log(elements.mainContent)
+    elements.mainContent.style.backgroundImage = "url('" + url + "')"
+    elements.mainContent.style.backgroundSize =  "auto " + width + "%"
+}
 async function updateBalance(percentageArray, visualOnly) {
     for (let i = 0; i < elements.bars.length; i++) {
         let season = elements.bars[i]
@@ -22,12 +41,48 @@ async function updateBalance(percentageArray, visualOnly) {
     }
     
     // update global percentages if not just visual
-    if (!visualOnly) {
-        
-        seasons.spring = percentageArray[0]
-        seasons.summer = percentageArray[1]
-        seasons.autumn = percentageArray[2]
-        seasons.winter = percentageArray[3]
+    if (visualOnly) { return }
+
+    seasons.spring = percentageArray[0]
+    seasons.summer = percentageArray[1]
+    seasons.autumn = percentageArray[2]
+    seasons.winter = percentageArray[3]
+
+    // Convert strings to numbers
+    const numbersArray = percentageArray.map(Number);
+
+    // Create an array of objects containing the original index and value
+    const indexedValues = numbersArray.map((value, index) => ({ value, index }));
+
+    // sort the array to find the lighest and lowest value
+    indexedValues.sort((a, b) => b.value - a.value);
+
+    // Get the highest and lowest values
+    const highestValue = indexedValues[0].value;
+    const lowestValue = indexedValues[indexedValues.length - 1].value;
+
+    const diffirence = 10;
+
+    if (highestValue - lowestValue < diffirence) {
+        // Set the background to the balance image
+        setBackground(seasons.balancedImage.url, seasons.balancedImage.backgroundWidth);
+    } else {
+        // Set the background based on the highest value
+        const highestIndex = indexedValues[0].index;
+        switch (highestIndex) {
+            case 0:
+                setBackground(seasons.springImage.url, seasons.springImage.backgroundWidth);
+                break;
+            case 1:
+                setBackground(seasons.summerImage.url, seasons.summerImage.backgroundWidth);
+                break;
+            case 2:
+                setBackground(seasons.autumnImage.url, seasons.autumnImage.backgroundWidth);
+                break;
+            case 3:
+                setBackground(seasons.winterImage.url, seasons.winterImage.backgroundWidth);
+                break;
+        }
     }
 
     await sleep (2000)
@@ -45,7 +100,7 @@ async function fluctuate(range) {
                              seasons.summer * getRandomArbitrary(1-range,1+range),
                              seasons.autumn * getRandomArbitrary(1-range,1+range), 
                              seasons.winter * getRandomArbitrary(1-range,1+range)], true)
-        await sleep(10)
+        await sleep(400)
     }
 }
 
@@ -74,7 +129,7 @@ async function loadSheetData() {
         newAutumn = data.values[2][1]
         newWinter = data.values[3][1]
         
-        updateBalance([newSpring,newSummer,newAutumn,newWinter])
+        updateBalance([newSpring,newSummer,newAutumn,newWinter], false)
     }) 
 }
 
@@ -83,5 +138,5 @@ if (!localStorage.getItem("api-key")) {
     localStorage.setItem("api-key", apiKeyInput)
 }
 
-loadSheetData()
+//loadSheetData()
 fluctuate(.05)
