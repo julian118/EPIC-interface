94edab0ebbf260098e4cd44d903f6f7e48b6b84d
julian <juliankroes2206@gmail.com>
2024-02-19 22:53:30 +0100

    Refactor: variable changes to make code more readable and modular

--- app.js
+++ app.js
@@ -1,13 +1,11 @@
+
 elements = {
     bars : document.querySelectorAll('.bar'),
     totalBalance : document.querySelector('.totalBalance'),
-    mainContent : document.querySelector('.main-content')
+    mainContent : document.querySelector('.main-content'),
+    forecastMessage : document.querySelector('.forecast-message')
 }
-let seasons = {
-    spring : 100,
-    summer : 100,
-    autumn : 100,
-    winter : 100,
+const seasonImages = {
     balancedImage : {
         url : "images/balanced.webp",
         backgroundWidth : 180
@@ -29,6 +27,12 @@ let seasons = {
         backgroundWidth : 100
     }
 }
+let seasonData = {
+    spring : 100,
+    summer : 100,
+    autumn : 100,
+    winter : 100 
+}
 function setBackground(url, width) {
     console.log(elements.mainContent)
     elements.mainContent.style.backgroundImage = "url('" + url + "')"
@@ -43,10 +47,10 @@ async function updateBalance(percentageArray, visualOnly) {
     // update global percentages if not just visual
     if (visualOnly) { return }
 
-    seasons.spring = percentageArray[0]
-    seasons.summer = percentageArray[1]
-    seasons.autumn = percentageArray[2]
-    seasons.winter = percentageArray[3]
+    seasonData.spring = percentageArray[0]
+    seasonData.summer = percentageArray[1]
+    seasonData.autumn = percentageArray[2]
+    seasonData.winter = percentageArray[3]
 
     // Convert strings to numbers
     const numbersArray = percentageArray.map(Number);
@@ -61,26 +65,27 @@ async function updateBalance(percentageArray, visualOnly) {
     const highestValue = indexedValues[0].value;
     const lowestValue = indexedValues[indexedValues.length - 1].value;
 
-    const diffirence = 10;
+    const difference = 10;
 
-    if (highestValue - lowestValue < diffirence) {
+    if (highestValue - lowestValue < difference) {
         // Set the background to the balance image
-        setBackground(seasons.balancedImage.url, seasons.balancedImage.backgroundWidth);
+        setBackground(seasonImages.balancedImage.url, seasonImages.balancedImage.backgroundWidth);
     } else {
         // Set the background based on the highest value
         const highestIndex = indexedValues[0].index;
+
         switch (highestIndex) {
             case 0:
-                setBackground(seasons.springImage.url, seasons.springImage.backgroundWidth);
+                setBackground(seasonImages.springImage.url, seasonImages.springImage.backgroundWidth);
                 break;
             case 1:
-                setBackground(seasons.summerImage.url, seasons.summerImage.backgroundWidth);
+                setBackground(seasonImages.summerImage.url, seasonImages.summerImage.backgroundWidth);
                 break;
             case 2:
-                setBackground(seasons.autumnImage.url, seasons.autumnImage.backgroundWidth);
+                setBackground(seasonImages.autumnImage.url, seasonImages.autumnImage.backgroundWidth);
                 break;
             case 3:
-                setBackground(seasons.winterImage.url, seasons.winterImage.backgroundWidth);
+                setBackground(seasonImages.winterImage.url, seasonImages.winterImage.backgroundWidth);
                 break;
         }
     }
@@ -96,10 +101,10 @@ function getRandomArbitrary(min, max) {
 }
 async function fluctuate(range) {
     while (true) {
-        await updateBalance([seasons.spring * getRandomArbitrary(1-range,1+range),
-                             seasons.summer * getRandomArbitrary(1-range,1+range),
-                             seasons.autumn * getRandomArbitrary(1-range,1+range), 
-                             seasons.winter * getRandomArbitrary(1-range,1+range)], true)
+        await updateBalance([seasonData.spring * getRandomArbitrary(1-range,1+range),
+                             seasonData.summer * getRandomArbitrary(1-range,1+range),
+                             seasonData.autumn * getRandomArbitrary(1-range,1+range), 
+                             seasonData.winter * getRandomArbitrary(1-range,1+range)], true)
         await sleep(400)
     }
 }
@@ -109,7 +114,7 @@ async function fluctuate(range) {
 async function loadSheetData() {
     const SHEETID = '1DkKHbezIZ6JiIocJJM0PqOH3MgwLdknwgzckR78dyhM'
     const SHEETNAME = 'API'
-    const CELLRANGE = 'A2:B5'
+    const CELLRANGE = 'A2:B6'
     let  APIKEY = ''
     
     // gets the users api key from the input
@@ -124,19 +129,29 @@ async function loadSheetData() {
       return response.json();
     })
     .then((data) => {
+        // asigning spreadsheet data to variables
         newSpring = data.values[0][1]
         newSummer = data.values[1][1]
         newAutumn = data.values[2][1]
         newWinter = data.values[3][1]
+        forecastMessage = data.values[4][1]
         
+        // visualy updating the fetched data
+        elements.forecastMessage.innerHTML = forecastMessage
         updateBalance([newSpring,newSummer,newAutumn,newWinter], false)
     }) 
 }
+function promptApiKey() {
+    let apiKeyInput = prompt("enter your api key")
+    if (apiKeyInput) {
+        localStorage.setItem("api-key", apiKeyInput)
+    }   
+}
+
 
 if (!localStorage.getItem("api-key")) {
-    let apiKeyInput = prompt("enter your api key")
-    localStorage.setItem("api-key", apiKeyInput)
+    promptApiKey()
 }
 
 //loadSheetData()
-fluctuate(.05)
+//fluctuate(.05)
